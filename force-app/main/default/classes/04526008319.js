43910b722680a39e0db27180c8a78a04c055f4d9
Malathi Subramaniyam <malathinirmal@gmail.com>
2025-01-23 12:05:28 -0500

    Common Files for reference

---
+++ force-app/main/default/classes/04526008319.js
@@ -0,0 +1,8 @@
+04526008319
+ "AlertName__c":'TestAlert',
+ "Alert_Type__c":'declinedTransaction',
+ "Amount__c":30000.0,
+ "EmailRecipients__c":'malathi@gmail.com',
+ "Merchant_Type__c":'online',
+ "Notes__c":'',
+ "Transcation_Type__c":'purchases'
